2d9cedbccc6696e428e989cf6426858ebece8542
Cheng ge <3171785486@qq.com>
2026-04-18 23:19:15 +0800

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,5 @@
+from services.model_router import call_llm, safe_call
+from services.qwen_client import call_qwen
+from services.deepseek_client import call_deepseek
 
+__all__ = ["call_llm", "safe_call", "call_qwen", "call_deepseek"]
